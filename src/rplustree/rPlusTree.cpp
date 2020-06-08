884f8bfffe5ce925d99da13b8a1351699ffea1d6
Pei Lin Li <pl3li@uwaterloo.ca>
2020-06-07 22:26:39 -0400

    insert and chooseLeaves function in insertition algorithm.

--- src/rplustree/rPlusTree.cpp
+++ src/rplustree/rPlusTree.cpp
@@ -17,7 +17,31 @@ void RPlusTree::adjustTree(RPlusTreeNode* n, RPlusTreeNode* nn)
 
 void RPlusTree::chooseLeaves(RPlusTreeNode* node, Rectangle& givenRectangle, std::vector<RPlusTreeNode*>& leaves)
 {
-	// TOOD
+	if (node->isLeaf()) {
+		leaves.push_back(node);
+		return;
+	}
+
+	bool overlap = false;
+	for (auto & child : node->children) {
+		if (child->boundingBox.computeExpansionArea(givenRectangle) == 0.0f) {
+			chooseLeaves(child, givenRectangle, leaves);
+			overlap = true;
+		}
+	}
+
+	if (!overlap) {
+		// Find the bounding box with least required expansion/overlap
+		RPlusTreeNode* chosenChild = node->children.at(0);
+		float smallestExpansionArea = node->children.at(0)->boundingBox.computeExpansionArea(givenRectangle);
+		for (auto & child : node->children) {
+			float testExpansionArea = child->boundingBox.computeExpansionArea(givenRectangle);
+			if (smallestExpansionArea > testExpansionArea) {
+				smallestExpansionArea = testExpansionArea;
+			}
+		}
+		chooseLeaves(chosenChild, givenRectangle, leaves);
+	}
 }
 
 Cost RPlusTree::sweep(std::vector<Rectangle>& rectangles, Orientation orientation)
@@ -55,7 +79,20 @@ std::vector<Point> RPlusTree::search(Rectangle requestedRectangle)
 
 void RPlusTree::insert(Rectangle givenRectangle)
 {
-	// TOOD
+	// choose the leaves where the data will go
+	std::vector<RPlusTreeNode*> leaves;
+	chooseLeaves(root, givenRectangle, leaves);
+
+	for (auto & leaf : leaves) {
+		leaf->data.push_back(givenRectangle);
+		// need to split leaf node
+		if (leaf->numEntries() > maxBranchFactor) {
+			Partition split = splitNode(leaf);
+			adjustTree(split.first, split.second);
+		} else {
+			adjustTree(leaf, nullptr);
+		}
+	}
 }
 
 void RPlusTree::remove(Point givenPoint)
