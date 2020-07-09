ee9b18e141a5f6514ccbc41ad64b5e05e8b27c6e
Pei Lin Li <pl3li@uwaterloo.ca>
2020-07-09 13:33:45 -0400

    fixup! added const to search functions.

--- src/include/index/index.h
+++ src/include/index/index.h
@@ -6,8 +6,8 @@
 
 class Index {
 public:
-	virtual std::vector<Point> search(Point requestedPoint) { throw std::exception(); };
-	virtual std::vector<Point> search(Rectangle requestedRectangle) { throw std::exception(); };
+	virtual std::vector<Point> search(Point requestedPoint) const { throw std::exception(); };
+	virtual std::vector<Point> search(Rectangle requestedRectangle) const { throw std::exception(); };
 	virtual void insert(Point givenPoint) { throw std::exception(); };
 	virtual void remove(Point givenPoint) { throw std::exception(); };
 	virtual unsigned checksum() { throw std::exception(); };
