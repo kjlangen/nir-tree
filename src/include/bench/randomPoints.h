9b084d1efd02fc12d21b2719a0feead28a736238
Kyle Langendoen <kjlangen@uwaterloo.ca>
2021-01-27 11:08:14 -0500

    Added P-Haze data as cluster data and added Bit data as skewed data. Added in option to turn on/off visualization. Added help text for options.

--- src/include/bench/randomPoints.h
+++ src/include/bench/randomPoints.h
@@ -13,6 +13,10 @@
 #include <rplustree/rplustree.h>
 #include <nirtree/nirtree.h>
 
+const unsigned BitDataSize = 60000;
+const unsigned BitQuerySize = 3164;
+const unsigned HazeDataSize = 65365;
+const unsigned HazeQuerySize = 3244;
 const unsigned CaliforniaDataSize = 1888012;
 const unsigned CaliforniaQuerySize = 5974;
 const unsigned BiologicalDataSize = 11958999;
@@ -24,10 +28,18 @@ enum BenchType {UNIFORM, SKEW, CLUSTER, CALIFORNIA, BIOLOGICAL, FOREST};
 enum TreeType {R_TREE, R_PLUS_TREE, NIR_TREE};
 
 Point *generateUniform(unsigned benchmarkSize, unsigned seed);
-Point *generateSkewed(unsigned benchmarkSize, unsigned seed, double skewFactor);
-Point *generateClustered(unsigned benchmarkSize, unsigned seed, unsigned clusterCount);
+Point *generateBits();
+Point *generateHaze();
 Point *generateCalifornia();
 Point *generateBiological();
+Point *generateForest();
+
+Rectangle *generateRectangles(unsigned benchmarkSize, unsigned seed, unsigned rectanglesSize);
+Rectangle *generateBitRectangles();
+Rectangle *generateHazeRectangles();
+Rectangle *generateCaliRectangles();
+Rectangle *generateBioRectangles();
+Rectangle *generateForestRectangles();
 
 void randomPoints(std::map<std::string, unsigned> &configU, std::map<std::string, double> &configF);
 
