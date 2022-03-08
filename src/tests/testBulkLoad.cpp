ce9f2b43ca97972b9a6a11c1dc6407aba7974f77
Brad Glasbergen <bglasber@gmail.com>
2022-03-08 14:32:17 -0500

    Bugfixes

--- src/tests/testBulkLoad.cpp
+++ src/tests/testBulkLoad.cpp
@@ -11,7 +11,9 @@
 #include <stdio.h>
 
 
-std::vector<Point> generate_points(std::vector<Point> all_points, unsigned size) {
+std::vector<Point> generate_points( unsigned size ) {
+    std::vector<Point> all_points;
+    all_points.reserve( size );
     std::optional<Point> next;
 
     BenchTypeClasses::Uniform::size = size;
@@ -37,23 +39,16 @@ void generate_tree(nirtreedisk::NIRTreeDisk<5,9,nirtreedisk::ExperimentalStrateg
             (unsigned) branch_factor, (unsigned) 0, (unsigned)
             max_depth, nullptr );
     tree->root = ret.first;
-
-    REQUIRE(tree->validate());
-}
-
-void remove_tree(std::string filename) {
-    std::remove(filename.c_str());
-    std::remove("file_backing.db");
 }
 
 TEST_CASE("gen_tree: test_uniform_generation") {
+    unlink( "bulkloaded_tree_test.txt" );
     std::string file_name = "bulkloaded_tree_test.txt";
     nirtreedisk::NIRTreeDisk<5,9,nirtreedisk::ExperimentalStrategy> *tree =  new
             nirtreedisk::NIRTreeDisk<5,9,nirtreedisk::ExperimentalStrategy>(
                     40960UL*20000UL, file_name );
     unsigned size = 81;
-    std::vector<Point> points;
-    generate_points(points, size);
+    std::vector<Point> points = generate_points(size);
     generate_tree(tree, points, 9);
 
     for( Point p : points ) {
@@ -61,17 +56,17 @@ TEST_CASE("gen_tree: test_uniform_generation") {
         REQUIRE(out.size() == 1);
     }
     delete tree;
-    remove_tree(file_name);
+    unlink( "bulkloaded_tree_test.txt" );
 }
 
 TEST_CASE("gen_tree: test_non_perfect_depth") {
+    unlink( "test_non_perfect_depth.txt" );
     std::string file_name = "test_non_perfect_depth.txt";
     nirtreedisk::NIRTreeDisk<5,9,nirtreedisk::ExperimentalStrategy> *tree =  new
             nirtreedisk::NIRTreeDisk<5,9,nirtreedisk::ExperimentalStrategy>(
                     4096UL*20, file_name );
     unsigned size = 1203;
-    std::vector<Point> points;
-    generate_points(points, size);
+    std::vector<Point> points = generate_points(size);
     generate_tree(tree, points, 9);
 
     for( Point p : points ) {
@@ -79,10 +74,12 @@ TEST_CASE("gen_tree: test_non_perfect_depth") {
         REQUIRE(out.size() == 1);
     }
     delete tree;
-    remove_tree(file_name);
+    unlink( "test_non_perfect_depth.txt" );
+
 }
 
 TEST_CASE("gen_tree: test_same_y") {
+    unlink( "test_same_y.txt" );
     std::string file_name = "test_same_y.txt";
     nirtreedisk::NIRTreeDisk<5,9,nirtreedisk::ExperimentalStrategy> *tree =  new
             nirtreedisk::NIRTreeDisk<5,9,nirtreedisk::ExperimentalStrategy>(
@@ -102,10 +99,11 @@ TEST_CASE("gen_tree: test_same_y") {
         REQUIRE(out.size() == 1);
     }
     delete tree;
-    remove_tree(file_name);
+    unlink( "test_same_y.txt" );
 }
 
 TEST_CASE("gen_tree: test_same_x") {
+    unlink( "test_same_x.txt" );
     std::string file_name = "test_same_x.txt";
     nirtreedisk::NIRTreeDisk<5,9,nirtreedisk::ExperimentalStrategy> *tree =  new
             nirtreedisk::NIRTreeDisk<5,9,nirtreedisk::ExperimentalStrategy>(
@@ -125,17 +123,17 @@ TEST_CASE("gen_tree: test_same_x") {
         REQUIRE(out.size() == 1);
     }
     delete tree;
-    remove_tree(file_name);
+    unlink( "test_same_x.txt" );
 }
 
 TEST_CASE("gen_tree: underfill_tree") {
+    unlink( "underfill_tree.txt" );
     std::string file_name = "underfill_tree.txt";
     nirtreedisk::NIRTreeDisk<5,9,nirtreedisk::ExperimentalStrategy> *tree =  new
             nirtreedisk::NIRTreeDisk<5,9,nirtreedisk::ExperimentalStrategy>(
                     4096UL*200UL, file_name);
     unsigned size = 8;
-    std::vector<Point> points;
-    generate_points(points, size);
+    std::vector<Point> points = generate_points(size);
     generate_tree(tree, points, 9);
 
     for( Point p : points ) {
@@ -143,6 +141,6 @@ TEST_CASE("gen_tree: underfill_tree") {
         REQUIRE(out.size() == 1);
     }
     delete tree;
-    remove_tree(file_name);
+    unlink( "underfill_tree.txt" );
 }
 
